4534dace6cebab24b4d7bcb771d861b7e453aefe
LE404An40\uer <411422259@m365.fju.edu.tw>
2023-12-19 10:02:26 +0800

    抽出五張撲克牌

--- src/_1219_1.java
+++ src/_1219_1.java
@@ -1,8 +1,18 @@
-
+import java.util.*;
 public class _1219_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int [] poker = new int [52]; //用來管制目前已抽出的亂數
+		int count = 0; //目前已抽出不重複的亂數數量
+		
+		do {
+			int temp=(int)(Math.random()*52);
+			if(poker[temp]==0) {
+				System.out.println(temp);
+				count++;
+			}
+		}while(count!=5); //迴圈一直反覆到成功抽出五個不重複的亂數
 
 	}
 
